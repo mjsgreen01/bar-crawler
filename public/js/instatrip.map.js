cac6e5ebc42cc841606ac5f1adcf601ca39107b2
mjsgreen01 <mjsgreen01@gmail.com>
2015-09-18 17:18:41 -0400

    [feat] display message until a bar is clicked

--- public/js/instatrip.map.js
+++ public/js/instatrip.map.js
@@ -2,6 +2,9 @@ angular.module('instatrip.map',[])
   .controller('mapCtrl', mapCtrl);
 
 function mapCtrl ($scope, Getdata, $rootScope){
+  $scope.init = function(){
+    $scope.barClicked = false;
+  };
   
   function randomIntFromInterval(min,max){
       return Math.floor(Math.random()*(max-min+1)+min);
@@ -21,6 +24,12 @@ function mapCtrl ($scope, Getdata, $rootScope){
 
   setRandomInterval();
 
+  $scope.$watch(function(scope) {
+    return Getdata.getImages();
+  }, function(newValue, oldValue) {
+    newValue === undefined || newValue.length === 0 ? $scope.barClicked = false : $scope.barClicked = true;
+  });
+
   $scope.showUber = false;
 
   $scope.getmap = Getdata.getmap;
@@ -29,5 +38,6 @@ function mapCtrl ($scope, Getdata, $rootScope){
     Getdata.getmap($rootScope.start, $rootScope.end, $rootScope.travelMethod);
   };
 
+  $scope.init();
   $scope.makeMap();
 }
